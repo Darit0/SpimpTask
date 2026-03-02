29e45de1411bc0835fc22dbe7e205c8ea7e36523
Ershov Dariy <ershovdar@yandex.ru>
2026-03-02 17:47:47 +0300

    add test examples

--- clojure/FirstLabNineTask.clj
+++ clojure/FirstLabNineTask.clj
@@ -9,3 +9,11 @@
     (sequential? coll)
       (reduce + 0 (map sum-nested coll))
     :else 0)) 
+
+(defn -main
+  [& args]
+  (println "=== Тестирование sum-nested ===")
+  (println "[1 2 3] →" (sum-nested [1 2 3]))
+  (println "[1 [2 3] [4 [5]]] →" (sum-nested [1 [2 3] [4 [5]]]))
+  (println "[] →" (sum-nested []))
+  (println "[1 \"x\" 2] →" (sum-nested [1 "x" 2]))
